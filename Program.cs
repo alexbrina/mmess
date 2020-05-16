90deb2e348dce13e9e3c39396568b3db210f25bc
Alexandre Brina <alexbrina@gmail.com>
2020-05-15 23:59:03 -0300

    Using auto recovery

--- Program.cs
+++ Program.cs
@@ -11,6 +11,13 @@ namespace ConsoleApp
 {
     public static class Program
     {
+        private static string Host;
+        private static int Port;
+        private static string User;
+        private static string Pass;
+        private static TimeSpan RecoveryInterval;
+        private static byte RecoveryAttempts;
+
         static void Main(string[] args)
         {
             var cmd = new RootCommand("RabbitMQ Mass Messaging");
@@ -20,6 +27,8 @@ namespace ConsoleApp
             cmd.AddGlobalOption(new Option<int>(new string[] { "--port", "-hp" }, () => -1));
             cmd.AddGlobalOption(new Option<string>(new string[] { "--user", "-u" }, () => "guest"));
             cmd.AddGlobalOption(new Option<string>(new string[] { "--pass", "-p" }, () => "guest"));
+            cmd.AddGlobalOption(new Option<int>(new string[] { "--recovery-interval", "-ri" }, () => 15000, "Network recovery interval (milliseconds)."));
+            cmd.AddGlobalOption(new Option<byte>(new string[] { "--recovery-attempts", "-ra" }, () => 30, "Network recovery attempts (0 to 255)."));
 
             // Publish command options
             var pub = new Command("publish");
@@ -29,7 +38,7 @@ namespace ConsoleApp
             pub.AddOption(oExchange);
             pub.AddOption(new Option<int>(new string[] { "--interval", "-i" }, () => 1000, "Publish sleep interval (milliseconds)."));
             pub.AddOption(new Option<int>(new string[] { "--size", "-s" }, () => 1024, "Message size (bytes)."));
-            pub.Handler = CommandHandler.Create<string, int, int, string, int, string, string>(Publish);
+            pub.Handler = CommandHandler.Create<string, int, int>(Publish);
             cmd.AddCommand(pub);
 
             // Subscribe command options
@@ -39,45 +48,50 @@ namespace ConsoleApp
             oQueues.Argument.Arity = ArgumentArity.OneOrMore;
             sub.AddOption(oQueues);
             sub.AddOption(new Option<int>(new string[] { "--interval", "-i" }, () => 1000, "Handling sleep interval (milliseconds)."));
-            sub.Handler = CommandHandler.Create<string[], int, string, int, string, string>(Subscribe);
+            sub.Handler = CommandHandler.Create<string[], int>(Subscribe);
             cmd.AddCommand(sub);
+
+            var arguments = cmd.Parse(args);
+            Host = arguments.CommandResult.ValueForOption<string>("--host");
+            Port = arguments.CommandResult.ValueForOption<int>("--port");
+            User = arguments.CommandResult.ValueForOption<string>("--user");
+            Pass = arguments.CommandResult.ValueForOption<string>("--pass");
+            RecoveryInterval = TimeSpan.FromMilliseconds(Math.Abs(arguments.CommandResult.ValueForOption<int>("--recovery-interval")));
+            RecoveryAttempts = arguments.CommandResult.ValueForOption<byte>("--recovery-attempts");
 
             // Invoke execution
             cmd.InvokeAsync(args).Wait();
         }
 
-        public static void Publish(string exchange, int interval, int size, string host, int port, string user, string pass)
+        public static void Publish(string exchange, int interval, int size)
         {
-            var cc = CreateConnection(host, port, user, pass);
+            var cc = CreateConnection();
             var cn = CreateChannel(cc);
 
             size = Math.Abs(size);
             interval = Math.Abs(interval);
             var i = 1;
-            Console.WriteLine("Press ESC to stop");
 
-            do
+            // Wait until key press
+            while (!Console.KeyAvailable)
             {
-                while (! Console.KeyAvailable)
+                try
                 {
-                    try
-                    {
-                        var message = $"Message {DateTimeOffset.Now:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'fffffff}";
-                        var bp = cn.CreateBasicProperties();
-                        bp.DeliveryMode = 2; // persistent
-                        cn.BasicPublish(exchange, "", bp, Encoding.UTF8.GetBytes((message + "\n" + new string('$', size)).Substring(0, size)));
-                        cn.WaitForConfirmsOrDie(TimeSpan.FromMilliseconds(15000));
-                        Console.WriteLine($"{message} sent! (#{i++})");
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine($"Exception :: {ex.Message}\n{ex.InnerException?.Message}");
-                        (cc, cn) = TryRecover(cc, cn, host, port, user, pass);
-                    }
-
-                    Thread.Sleep(interval);
+                    var message = $"Message {DateTimeOffset.Now:yyyy'-'MM'-'dd' 'HH':'mm':'ss'.'fffffff}";
+                    var bp = cn.CreateBasicProperties();
+                    bp.DeliveryMode = 2; // persistent
+                    cn.BasicPublish(exchange, "", bp, Encoding.UTF8.GetBytes((message + "\n" + new string('$', size)).Substring(0, size)));
+                    cn.WaitForConfirmsOrDie(TimeSpan.FromMilliseconds(15000));
+                    Console.WriteLine($"{message} sent! (#{i++})");
                 }
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Publish Exception :: " + ex.GetType());
+                    cn = WaitAutoRecovery(cc, cn);
+                }
+
+                Thread.Sleep(interval);
+            }
 
             // Closing
             if (cn?.IsOpen ?? false) cn.Close();
@@ -87,23 +101,30 @@ namespace ConsoleApp
             cc?.Dispose();
         }
 
-        public static void Subscribe(string[] queues, int interval, string host, int port, string user, string pass)
+        public static void Subscribe(string[] queues, int interval)
         {
-            var cc = CreateConnection(host, port, user, pass);
-            var cn = CreateChannel(cc);
-
+            var Exiting = false;
             var consumersToQueues = new Dictionary<string, string>();
-            var cs = new EventingBasicConsumer(cn);
 
-            interval = Math.Abs(interval);
+            var cc = CreateConnection();
+            var cn = CreateChannel(cc);
+            var cs = CreateConsumer(cn);
+            cs.Received += Handler;
 
-            // Message handler
-            cs.Received += (sender, ea) =>
+            cs.ConsumerCancelled += (sender, ea) =>
             {
-                try
+                if (!Exiting)
                 {
-                    Monitor.Enter(consumersToQueues);
+                    WaitAutoRecovery(cc, cs.Model, RecoveryAttempts);
+                }
+            };
 
+            void Handler(object sender, BasicDeliverEventArgs ea)
+            {
+                interval = Math.Abs(interval);
+
+                lock (consumersToQueues)
+                {
                     var msg = Encoding.UTF8.GetString(ea.Body.ToArray());
                     if (string.IsNullOrEmpty(msg))
                     {
@@ -112,63 +133,71 @@ namespace ConsoleApp
                     var nli = msg.IndexOf("\n");
                     msg = msg.Substring(0, nli < 0 ? msg.Length : nli);
 
-                    if (consumersToQueues.TryGetValue(ea.ConsumerTag, out string queue))
+                    if (consumersToQueues.TryGetValue(ea.ConsumerTag, out string queue)
+                        && cs.Model == cn)
                     {
-                        Console.WriteLine($"{msg} received from {queue}! (#{ea.DeliveryTag})");
                         Thread.Sleep(interval);
-                        cn.BasicAck(ea.DeliveryTag, false);
+                        Console.Write($"{msg} received from {queue}! (#{ea.DeliveryTag})");
+                        if  (cs.Model.IsOpen)
+                        {
+                            cs.Model.BasicAck(ea.DeliveryTag, false);
+                            Console.WriteLine(" ... ack ok.");
+                        }
+                        else
+                        {
+                            Console.WriteLine($" ... but not ack, channel is closed.");
+                        }
                     }
                     else
                     {
-                        Console.WriteLine($"{msg} received but was ignored because subscription was stopped! (#{ea.DeliveryTag})");
+                        Console.WriteLine($"{msg} received! (#{ea.DeliveryTag}) ... ignored because channel was stopping.");
                     }
                 }
-                catch (Exception ex)
+            }
+
+            void StartConsuming(IModel cn, EventingBasicConsumer cs)
+            {
+                Console.WriteLine($"{nameof(StartConsuming)}...");
+
+                lock (consumersToQueues)
                 {
-                    Console.WriteLine($"Exception :: {ex.Message}\n{ex.InnerException?.Message}");
                     if (cn?.IsOpen ?? false)
                     {
-                        cn.BasicNack(ea.DeliveryTag, false, true);
+                        foreach (var queue in queues)
+                        {
+                            var tag = cn.BasicConsume(queue, false, cs);
+                            consumersToQueues.Add(tag, queue);
+                        }
                     }
-                    (cc, cn) = TryRecover(cc, cn, host, port, user, pass);
-                }
-                finally
-                {
-                    Monitor.Exit(consumersToQueues);
-                }
-            };
-
-            // Start consuming
-            lock (consumersToQueues)
-            {
-                foreach (var queue in queues)
-                {
-                    var tag = cn.BasicConsume(queue, false, cs);
-                    consumersToQueues.Add(tag, queue);
                 }
             }
 
-            // Wait until Esc key press
-            Console.WriteLine("Press ESC to stop");
-            do
+            void StopConsuming(IModel cn)
             {
-                while (!Console.KeyAvailable)
-                {
-                    Thread.Sleep(500);
-                }
-            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+                Console.WriteLine($"{nameof(StopConsuming)}...");
 
-            // Stop consuming
-            lock (consumersToQueues)
-            {
-                if (cn?.IsOpen ?? false)
+                lock (consumersToQueues)
                 {
-                    foreach (var tag in consumersToQueues)
+                    if (cn?.IsOpen ?? false)
                     {
-                        cn.BasicCancel(tag.Key);
+                        foreach (var tag in consumersToQueues)
+                        {
+                            cn.BasicCancel(tag.Key);
+                            Console.WriteLine($"{nameof(StopConsuming)} {tag.Key} stoped.");
+                        }
                     }
+
+                    consumersToQueues.Clear();
                 }
             }
+
+            StartConsuming(cn, cs);
+            while (!Console.KeyAvailable && !Exiting)
+            {
+                Thread.Sleep(500);
+            }
+            Exiting = true;
+            StopConsuming(cn);
 
             // Closing
             if (cn?.IsOpen ?? false) cn.Close();
@@ -178,16 +207,17 @@ namespace ConsoleApp
             cc?.Dispose();
         }
 
-        private static IConnection CreateConnection(string host, int port, string user, string pass)
+        private static IConnection CreateConnection()
         {
             var cf = new ConnectionFactory
             {
-                HostName = host,
-                Port = port,
-                UserName = user,
-                Password = pass,
+                HostName = Host,
+                Port = Port,
+                UserName = User,
+                Password = Pass,
                 AutomaticRecoveryEnabled = true,
-                RequestedHeartbeat = TimeSpan.FromSeconds(20)
+                NetworkRecoveryInterval = RecoveryInterval,
+                RequestedHeartbeat = RecoveryInterval
             };
 
             var cc = cf.CreateConnection();
@@ -195,7 +225,7 @@ namespace ConsoleApp
             cc.ConnectionBlocked += (sender, ea) => Console.WriteLine($"Connection Event :: ConnectionBlocked :: {ea.Reason}");
             cc.ConnectionUnblocked += (sender, ea) => Console.WriteLine($"Connection Event :: ConnectionUnblocked");
             cc.ConnectionShutdown += (sender, ea) => Console.WriteLine($"Connection Event :: ConnectionShutdown :: {ea.ReplyCode} - {ea.ReplyText}");
-            cc.CallbackException += (sender, ea) => Console.WriteLine($"Connection Event :: CallbackException :: {ea.Exception}");
+            cc.CallbackException += (sender, ea) => Console.WriteLine($"Connection Event :: CallbackException :: " + ea.Exception);
 
             Console.WriteLine("New Connection Created!");
 
@@ -209,7 +239,7 @@ namespace ConsoleApp
             cn.BasicReturn += (sender, ea) => Console.WriteLine($"Channel Event :: BasicReturn :: {ea.ReplyCode} - {ea.ReplyText}");
             cn.BasicNacks += (sender, ea) => Console.WriteLine($"Channel Event :: BasicNacks :: DeliveryTag {ea.DeliveryTag}");
             cn.ModelShutdown += (sender, ea) => Console.WriteLine($"Channel Event :: ModelShutdown :: {ea.ReplyCode} - {ea.ReplyText}");
-            cn.CallbackException += (sender, ea) => Console.WriteLine($"Channel Event :: CallbackException :: {ea.Exception}");
+            cn.CallbackException += (sender, ea) => Console.WriteLine($"Channel Event :: CallbackException :: " + ea.Exception);
 
             cn.ConfirmSelect();
             cn.BasicQos(0, 10, true);
@@ -219,37 +249,41 @@ namespace ConsoleApp
             return cn;
         }
 
-        private static (IConnection, IModel) TryRecover(IConnection cc, IModel cn, string host, int port, string user, string pass, byte retry = 1)
+        private static EventingBasicConsumer CreateConsumer(IModel cn)
         {
-            if (retry > 10)
-            {
-                throw new TimeoutException("Max retries reached, sorry.");
-            }
+            var cs = new EventingBasicConsumer(cn);
 
-            try
-            {
-                if (!cc?.IsOpen ?? true)
-                {
-                    cc?.Dispose();
-                    cc = CreateConnection(host, port, user, pass);
-                }
+            cs.Registered += (sender, ea) => Console.WriteLine($"Consumer Event :: Registered :: {string.Join(", ", ea.ConsumerTags)}");
+            cs.Unregistered += (sender, ea) => Console.WriteLine($"Consumer Event :: Unregistered :: {string.Join(", ", ea.ConsumerTags)}");
+            cs.ConsumerCancelled += (sender, ea) => Console.WriteLine($"Consumer Event :: ConsumerCancelled :: {string.Join(", ", ea.ConsumerTags)}");
+            cs.Shutdown += (sender, ea) => Console.WriteLine($"Consumer Event :: Shutdown :: {ea.Initiator} - {ea.ReplyCode} - {ea.ReplyText}");
 
-                if (!cn?.IsOpen ?? true)
-                {
-                    cn?.Dispose();
-                    cn = CreateChannel(cc);
-                }
-            }
-            catch (Exception ex)
+            Console.WriteLine("New Consumer Created!");
+
+            return cs;
+        }
+
+        private static IModel WaitAutoRecovery(IConnection cc, IModel cn, byte retry = 1)
+        {
+            if (!cc.IsOpen)
             {
-                Console.WriteLine($"Exception :: {ex.Message}\n{ex.InnerException?.Message}");
-                var wait = Convert.ToInt32((Math.Pow(2,retry) - 1));
-                Console.WriteLine($"Retry :: #{retry} :: Waiting {wait} seconds before retry");
+                if (retry > RecoveryAttempts)
+                {
+                    Console.WriteLine("Max wait times reached. Sorry, I tried.");
+                    Environment.Exit(1);
+                }
+                var wait = Convert.ToInt32(RecoveryInterval.TotalSeconds);
+                Console.WriteLine($"{nameof(WaitAutoRecovery)} :: #{retry}/{RecoveryAttempts} :: Waiting {wait} seconds before checking connection recovery.");
                 Thread.Sleep(wait * 1000);
-                (cc, cn) = TryRecover(cc, cn, host, port, user, pass, ++retry);
+                WaitAutoRecovery(cc, cn, ++retry);
+            }
+            else if (!cn.IsOpen)
+            {
+                Console.WriteLine($"{nameof(WaitAutoRecovery)} :: Connection restored but channel is closed.");
+                return CreateChannel(cc);
             }
 
-            return (cc, cn);
+            return cn;
         }
     }
 }
